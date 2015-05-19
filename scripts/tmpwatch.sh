2887da41bd7b7296a24bb0996e4fb0659530cec9
Nithin Nayak Sujir <nithinsujir@gmail.com>
2015-05-19 10:48:07 -0700

    tmpwatch.sh

---
+++ scripts/tmpwatch.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+dirs=(
+	/data/workspaces/build0
+	/data2/workspaces/build2
+	/data2/workspaces/build3
+	/data3/workspaces/build1
+	/data3/workspaces/build4
+	/data3/workspaces/build5
+	/data3/workspaces/build6
+)
+
+for d in ${dirs[@]}; do
+	/usr/bin/sudo /usr/sbin/tmpwatch -mv 7d ${d}/distro_bld
+	/usr/bin/sudo /usr/sbin/tmpwatch -mv 7d ${d}/kbuild
+done
+

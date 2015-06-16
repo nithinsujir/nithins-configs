8b4489fd4b1abf6b6e61e36dedb9b0fd10cdd627
Nithin Nayak Sujir <nithinsujir@gmail.com>
2015-06-16 14:35:22 -0700

    rdesk

--- scripts/rdesk.sh
+++ scripts/rdesk.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env bash
+res=$(xdpyinfo  | grep dimensions | awk {'print $2;'})
+echo $res
+
+if [[ $res =~ 1600x900 ]]; then
+	res=1600x840
+else
+	res=1900x1050
+fi
+
 passwd=$(zenity --password --title=Authentication)
-rdesktop -g 1900x1050 -u tintri\\nsujir -p $passwd nsujir-win -K -x 0x80
+rdesktop -g $res -u tintri\\nsujir -p $passwd nsujir-win -K -x 0x80

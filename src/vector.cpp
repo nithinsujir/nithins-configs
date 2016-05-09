4de8ba9c6ae10966dc292677132c3837e4dbd88a
Nithin Nayak Sujir <nithinsujir@gmail.com>
2016-05-09 05:57:58 -0700

    netrwhist vector.cpp

---
+++ src/vector.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(void)
+{
+	vector<int> vec;
+	vec = {0, 1, 2};
+
+	cout << vec[0] << " " << vec[1] << " " << vec[2] << endl;
+
+	return 0;
+}
+
+

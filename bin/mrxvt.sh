05e185664f3c4f5f9b492745e287f649f28f997b
Nithin Nayak Sujir <nithinsujir@gmail.com>
2016-05-25 12:03:42 -0700

    mrxvt2

--- bin/mrxvt.sh
+++
@@ -1,1 +0,0 @@
-mrxvt +bt -bg beige  -fg black -xft -xftaa -xftfn Ubuntu\ Mono -xftsz 12 -aht -hb -sr -wd /home/nsujir -cf /home/nsujir/.mrxvtrc -sl 9999 -itabbg grey -itabfg black -hold 0 +showmenu -mvt 15 -xftpfn Ubuntu\ Mono -xftpsz 9

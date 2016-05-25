b51d59ff33ea20c62ca96b8d770313be07fd0fa6
Nithin Nayak Sujir <nithinsujir@gmail.com>
2016-05-25 12:30:39 -0700

    nvx.py dump kbuf

--- python/nvx.py
+++ python/nvx.py
@@ -2,35 +2,31 @@
 import mmap
 import os
 
-NVDIMM_DEV = '/dev/agigaram1'
-NVDIMM_SIZE = 1024 * 1024 * 1024
+NVDEV = '/dev/agigcntl'
+NVSIZE = 1024 * 1024 * 1024
 
 PATTERN1 = '1'
 PATTERN2 = '2'
 PATTERN3 = '3'
 
-f = open(NVDIMM_DEV, 'r+b')
-mm = mmap.mmap(f.fileno(), NVDIMM_SIZE,  flags = mmap.MAP_SHARED, prot = mmap.PROT_READ | mmap.PROT_WRITE)
+if not os.path.exists(NVDEV):
+    NVDEV = '/dev/umx0'
+
+f = open(NVDEV, 'r+b')
+mm = mmap.mmap(f.fileno(), NVSIZE,  flags = mmap.MAP_SHARED, prot = mmap.PROT_READ | mmap.PROT_WRITE)
 
 def write_pattern(pat):
     mm.seek(0)
 
-    for i in xrange((NVDIMM_SIZE / len(pat)) -  len(pat)):
+    for i in xrange((NVSIZE / len(pat)) -  len(pat)):
         mm.write(pat)
 
-pat1 = PATTERN1
-pat2 = PATTERN2
-pat3 = PATTERN3
+def dump(size):
+    mm.seek(0)
+    f = open('/var/log/nv.bin', 'w')
+    f.write(mm.read(size))
+    f.close()
 
-# Bump the strings upto 4k
-for i in xrange(12):
-    pat1 = pat1 * 2
-    pat2 = pat2 * 2
-    pat3 = pat3 * 2
-
-write_pattern(pat1)
-write_pattern(pat2)
-os.system('ipmitool power cycle')
-write_pattern(pat3)
+dump(NVSIZE)
 
 

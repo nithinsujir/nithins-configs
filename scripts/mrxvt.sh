af867d17f961004a49cb0695971af02d2c0aed0d
Nithin Nayak Sujir <nithinsujir@gmail.com>
2017-02-19 17:11:55 -0800

    mrxvt 9999

--- scripts/mrxvt.sh
+++ scripts/mrxvt.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-mrxvt +bt -bg beige  -fg black -xft -xftaa -xftfn Ubuntu\ Mono -xftsz 12 -aht -hb -sr -wd /home/nsujir -cf /home/nsujir/.mrxvtrc -sl 99999 -itabbg grey -itabfg black -hold 0 +showmenu -mvt 15 -xftpfn Ubuntu\ Mono -xftpsz 9 -geometry 80x30
+mrxvt +bt -bg beige  -fg black -xft -xftaa -xftfn Ubuntu\ Mono -xftsz 12 -aht -hb -sr -wd /home/nsujir -cf /home/nsujir/.mrxvtrc -sl 9999 -itabbg grey -itabfg black -hold 0 +showmenu -mvt 15 -xftpfn Ubuntu\ Mono -xftpsz 9 -geometry 80x30
 #mrxvt +bt -bg beige  -fg black -xft -xftaa -xftfn Inconsolata -xftsz 12 -aht -hb -sr -wd /home/nsujir -cf /home/nsujir/.mrxvtrc -sl 9999 -itabbg grey -itabfg black -hold 0 +showmenu -mvt 15 -xftpfn Ubuntu\ Mono -xftpsz 9 -geometry 80x30
 #mrxvt +bt -bg beige  -fg black -xft -xftaa -xftfn Liberation\ Mono -xftsz 11 -aht -hb -sr -wd /home/nsujir -cf /home/nsujir/.mrxvtrc -sl 9999 -itabbg grey -itabfg black -hold 0 +showmenu -mvt 15 -xftpfn Ubuntu\ Mono -xftpsz 9 -geometry 80x30
 #mrxvt +bt -bg beige  -fg black -xft -xftaa -xftfn Monospace -xftsz 10 -aht -hb -sr -wd /home/nsujir -cf /home/nsujir/.mrxvtrc -sl 9999 -itabbg grey -itabfg black -hold 0 +showmenu -mvt 15 -xftpfn Monospace -xftpsz 8
--- scripts/mrxvt3.sh
+++ scripts/mrxvt3.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-mrxvt +bt -bg beige  -fg black -xft -xftaa -xftfn Inconsolata -xftsz 12 -aht -hb -sr -wd /home/nsujir -cf /home/nsujir/.mrxvtrc -sl 99999 -itabbg grey -itabfg black -hold 0 +showmenu -mvt 15 -xftpfn Ubuntu\ Mono -xftpsz 9 -geometry 80x30 -at -e ssh -Y nsujir-ws
+mrxvt +bt -bg beige  -fg black -xft -xftaa -xftfn Inconsolata -xftsz 12 -aht -hb -sr -wd /home/nsujir -cf /home/nsujir/.mrxvtrc -sl 9999 -itabbg grey -itabfg black -hold 0 +showmenu -mvt 15 -xftpfn Ubuntu\ Mono -xftpsz 9 -geometry 80x30 -at -e ssh -Y nsujir-ws

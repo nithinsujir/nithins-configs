caf89e786ddb89afa398f62aadae5fcd7e8e8dc9
Nithin Nayak Sujir <nithinsujir@gmail.com>
2017-06-08 18:22:18 -0700

    mrxvt

--- scripts/mrxvt3.sh
+++ scripts/mrxvt3.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
-mrxvt +bt -bg beige  -fg black -xft -xftaa -xftfn Inconsolata -xftsz 12 -aht -hb -sr -wd /home/nsujir -cf /home/nsujir/.mrxvtrc -sl 9999 -itabbg grey -itabfg black -hold 0 +showmenu -mvt 15 -xftpfn Ubuntu\ Mono -xftpsz 9 -geometry 80x30 -at -e ssh -Y nsujir-ws
+SSH="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o loglevel=error"
+
+mrxvt +bt -bg beige  -fg black -xft -xftaa -xftfn Inconsolata -xftsz 12 -aht -hb -sr -wd /home/nsujir -cf /home/nsujir/.mrxvtrc -sl 9999 -itabbg grey -itabfg black -hold 0 +showmenu -mvt 15 -xftpfn Ubuntu\ Mono -xftpsz 9 -geometry 80x30 -at -e $SSH -Y nsujir-ws
